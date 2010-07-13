82e9030552bd6620b24fcd1ae0bf95d4f5949875
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-07-13 11:06:27 -0400

    forget blast, lets do fuzzy matching!

---
+++ search.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+''' 
+
+I decided to drop BLAST in favor of writing my own search algorithm.
+
+It's faster because there's no disk IO
+
+'''
+
+class Search:
+    ''' Representing a \"fuzzy\" search '''    
+    @classmethod
+    def find(self, template, query):
+        ''' The search algorithm.  Returns start '''
+        pass
+        
