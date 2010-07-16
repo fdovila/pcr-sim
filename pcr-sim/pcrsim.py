78dd31dd099b61de98cf84a7436ddd3656aeda06
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-07-16 14:58:43 -0400

    removed optparse options that dont do anything - this is a bad habit.

--- pcr-sim/pcrsim.py
+++ pcr-sim/pcrsim.py
@@ -13,8 +13,6 @@ def main(argv):
         description='pcrsim.py - Simulates PCR on a given input FASTA.', 
         usage = 'pcrsim.py -i infile -f forward_primer -r reverse_primer',
         version=VERSION)
-    parser.add_option("-v", "--verbose", dest="verbose", default=False,
-        action="store_false", help="Print Debugging Information")
     parser.add_option("-i", "--infile", dest="filename",
         help="Specify Input FASTA file", type="string")
     parser.add_option("-f", "--forward", dest="forward",
@@ -22,9 +20,6 @@ def main(argv):
         type="string")
     parser.add_option("-r", "--reverse", dest="reverse",
         help="Specify reverse primer (5' to 3')",
-        type="string")
-    parser.add_option("-n", "--notprimed", dest="notprimed",
-        help="Save not primed sequences to not_primed (optional)",
         type="string")
                     
     (options, args) = parser.parse_args()
@@ -37,10 +32,10 @@ def main(argv):
         hnotprimed = open(options.notprimed, 'w')
 
     # We can just recycle this object over and over again to save time!
-    S = Search(gc_score=2, at_score=1, miss_penn=1)
+    S = Search(match=1,  miss_pen=2)
 
     forward = options.forward.upper()
-    reverse = Dna('r', options.reverse.upper()).revcomp
+    reverse = options.reverse.upper()
 
     with open(options.filename, 'r') as handle:
         records = Fasta(handle)
@@ -49,12 +44,12 @@ def main(argv):
             S.scores.sort(key=lambda x: x[0])
             
             # Let's try printing all configurations!
-            print S.alignments          
+            #print S.alignments
             
             so_r = S.find(query=reverse)
-            print S.alignments
-            print '%5s-> <-%-5s/%5s\n%s' % \
-                (so_f[1], so_r[1], len(rec.seq), '-'*25)
+            #print S.alignments
+            print '%5s -> %-5s = %5s' % \
+                (so_f[1], so_r[1], len(rec.seq[so_f[1]:so_r[1]]))
 
             
 if __name__ == '__main__':
